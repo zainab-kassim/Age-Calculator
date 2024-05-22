74909fbdbcc7e66215bd1342c9a76feeca311646
zainab-kassim <kassimzainab11@gmail.com>
2024-05-22 18:30:09 +0100

    second commit

--- agecalc.js
+++ agecalc.js
@@ -3,6 +3,7 @@ let btn = document.getElementById("btn");
 let rst = document.getElementById("rst");
 let birth_month = document.getElementById("month");
 let birth_day = document.getElementById("day");
+let errorMessage = document.getElementById("error-message");
 
 // For current year
 const y = new Date();
@@ -14,24 +15,7 @@ let current_month = m.getMonth() + 1; // getMonth() returns 0-11,
 const d = new Date();
 let current_day = d.getDate(); // getDate() returns the day of the month
 
-// function daycalc() {
-//     let day;
-//     if (current_day >= parseInt(birth_day.value)) {
-//         day = current_day - parseInt(birth_day.value);
-//     } //for months with 30 days
-//     else if (current_day < parseInt(birth_day.value) && current_month === 1, 3, 5, 7, 8, 10, 12) {
-//         current_month--;
-//         day = 31 + current_day - parseInt(birth_day.value);
-//     }//for months with 30 days
-//     else if (current_day < parseInt(birth_day.value) && current_month === 4, 6, 9, 11){
-//             current_month--;
-//         day = 30 + current_day - parseInt(birth_day.value);
-//     }//for february
-//     else if (current_day < parseInt(birth_day.value) && current_month === 2){
-//         day = 28 + current_day - parseInt(birth_day.value)
-//     }
-//     return day;
-// }
+
 function daycalc() {
     let day;
     if (current_day >= parseInt(birth_day.value)) {
@@ -77,13 +61,37 @@ function yearcalc() {
     return age;
 }
 
-btn.addEventListener("click", function () {
-    let month = monthcalc();
-    let age = yearcalc();
-    let day = daycalc();
+function validateInputs() {
+    const yearValue = year.value;
 
-    document.getElementById("present-age").innerHTML = "You are " + age + "years " + month + "months and  " + day + "days old.";
-    btn.disabled=true;
+    const dayValue = birth_day.value;
+
+    if (yearValue.length !== 4 || isNaN(yearValue) || parseInt(yearValue) < 1000 || parseInt(yearValue) > 9999) {
+        errorMessage.textContent = "Please enter a valid 4-digit year.";
+        return false;
+    }
+
+    if (isNaN(dayValue) || parseInt(dayValue) < 1 || parseInt(dayValue) > 31) {
+        errorMessage.textContent = "Please enter a valid day between 1 and 31.";
+        return false;
+    }
+
+    errorMessage.textContent = ""; // Clear any previous error messages
+    return true
+}
+
+
+document.getElementById("container").addEventListener("submit", function (e) {
+
+    e.preventDefault();
+    if (validateInputs()) {
+        let month = monthcalc();
+        let age = yearcalc();
+        let day = daycalc();
+
+        document.getElementById("present-age").innerHTML = "You are " + age + "years " + month + "months and  " + day + "days old.";
+    }
+    btn.disabled = true;
 });
 
 rst.addEventListener("click", function () {
